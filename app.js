22906f21c33ce70c89526b015bc48148b126cf85
Bhargav Pathivada <107063758+Bhargav090@users.noreply.github.com>
2024-01-29 00:03:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ app.use(cors());
 
 // Mock data for nature images
 const natureImages = [
-  { id: 1, filename: 'img1.jpg', description: 'Beautiful landscape',
-    id: 2, filename: 'img2.jpg', description: 'high landscape'
+  { id: 1, filename: 'img1.jpg', description: 'Beautiful landscape'}
+  { id: 2, filename: 'img2.jpg', description: 'high landscape'
   },
   // Add more images as needed
 ];
