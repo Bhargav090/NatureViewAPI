b5a9dcb6e3d7672cbc5cd9f4a6144a3638c0ce36
Bhargav Pathivada <107063758+Bhargav090@users.noreply.github.com>
2024-01-29 00:16:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 
 // Endpoint to retrieve all nature images
 app.get('/all-images', (req, res) => {
-  res.json(natureImages);
+  res.json({ images: natureImages });
 });
 
 // Endpoint to add a new nature image
