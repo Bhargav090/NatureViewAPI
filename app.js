8d131dbb856479a3a7c3f5cc1e28efa096e3db4d
Bhargav Pathivada <107063758+Bhargav090@users.noreply.github.com>
2024-01-28 23:02:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -48,6 +48,11 @@ app.put('/images/:id', (req, res) => {
   res.json({ message: 'Image updated successfully', image: updatedImage });
 });
 
+// Define a simple route for the root path
+app.get('/', (req, res) => {
+  res.send('Welcome to the Nature Images API!');
+});
+
 app.listen(PORT, host, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
